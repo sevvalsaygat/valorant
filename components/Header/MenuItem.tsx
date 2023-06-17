eb140b206e1e0996c3acb7445023d6f8c4d20c80
sevvalsaygat <sevvallsstt@gmail.com>
2023-06-17 19:53:54 +0300

    feat: add chevron icon

--- components/Header/MenuItem.tsx
+++ components/Header/MenuItem.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import Link from 'next/link';
 
 import { Icons } from '@components';
@@ -16,15 +17,28 @@ type MenuItemPropTypes = {
 
 export default function MenuItem({ name, items }: MenuItemPropTypes) {
   const haveAnyItems = items.length > 0;
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const arrowIcon = isOpen ? (
+    <Icons.SvgArrowUp className="w-3 h-3 text-white absolute" />
+  ) : (
+    <Icons.SvgArrowDown className="w-3 h-3 text-white absolute" />
+  );
+
   return (
     <DropdownMenu as="div" className="relative inline-block text-left">
-      <div className="flex flex-row mr-4 items-center">
-        <DropdownMenu.Button className="text-gray-50 py-2 px-4 hover:bg-gray-100 rounded-7 font-bold">
-          {name}
-        </DropdownMenu.Button>
-
-        {haveAnyItems && <Icons.SvgArrowUp className="w-3 h-3 text-white" />}
-      </div>
+      <DropdownMenu.Button
+        className="text-gray-50 py-2 px-4 hover:bg-gray-100 rounded-7 font-bold flex flex-row items-center"
+        onClick={toggleDropdown}
+      >
+        {name}
+        {haveAnyItems && <div className="flex relative items-center">{arrowIcon}</div>}
+      </DropdownMenu.Button>
       <DropdownMenu.Items className="absolute ringt-0 mt-2 w-56 origin-top-right divide-y divide-gray-1">
         {items.map((item, index) => (
           <DropdownMenu.Item as="div" key={index}>
