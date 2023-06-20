7c863de9731c83783a5cb6d1312abff00b9aafb9
sevvalsaygat <sevvallsstt@gmail.com>
2023-06-20 22:35:41 +0300

    feat: agent page design

--- components/Agent/Item.tsx
+++ components/Agent/Item.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { useState } from 'react';
 
 import { AgentType } from '@types';
 
@@ -7,9 +8,34 @@ type AgentItemType = {
 };
 
 export default function AgentItem({ agent }: AgentItemType) {
+  const [hovered, setHovered] = useState(false);
+
   return (
-    <div className="border bg-red-500">
-      <Image height={500} width={500} alt={agent.displayName} src={agent.displayIcon!!} />
+    <div className="relative">
+      <div
+        className={`absolute top-0 left-0 right-0 bottom-0 hover:bg-red-150 hover:opacity-70 z-10 animate-slide ${
+          hovered ? 'opacity-70' : 'opacity-0'
+        }`}
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+        style={{ transition: 'opacity 0.1s' }}
+      >
+        <span
+          className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-xl font-bold ${
+            hovered ? 'opacity-100' : 'opacity-0'
+          }`}
+          style={{ transition: 'opacity 0.1s' }}
+        >
+          {agent.displayName}
+        </span>
+      </div>
+      <Image
+        className="shadow-lg border-transparent rounded-7"
+        height={500}
+        width={500}
+        alt={agent.displayName}
+        src={agent.fullPortrait!!}
+      />
     </div>
   );
 }
